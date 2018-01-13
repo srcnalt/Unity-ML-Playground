6de5b8f4df4a0cc836c36f82d6c452f6361345a8
Sercan Altundaş <sercan.altundas@gmail.com>
2018-01-14 01:57:35 +0200

    car map and sensor setup

--- unity-environment/Assets/ML-Agents/Examples/1 - Self Driving Car/Scripts/CarAgent.cs
+++ unity-environment/Assets/ML-Agents/Examples/1 - Self Driving Car/Scripts/CarAgent.cs
@@ -11,15 +11,21 @@ public class CarAgent : Agent
     [HideInInspector]
     public bool brake;
 
+    public SensoryRays sensors;
+
     public override List<float> CollectState()
     {
-        return base.CollectState();
+        List<float> states = new List<float>();
+
+        states.Add(sensors.colorLeft);
+        states.Add(sensors.colorMid);
+        states.Add(sensors.colorRight);
+
+        return states;
     }
 
     public override void AgentStep(float[] action)
     {
-        Debug.Log(action[0]);
-
         brake = false;
 
         switch((int)action[0])
@@ -28,25 +34,52 @@ public class CarAgent : Agent
                 if (vertical < 1)
                     vertical += Time.deltaTime;
                 break;
-
+            
             case 2:
-                if (horizontal < 1)
-                    horizontal += Time.deltaTime;
-                break;
-
-            case 3:
                 if (horizontal > -1)
                     horizontal -= Time.deltaTime;
                 break;
-
+            
+            case 3:
+                if (horizontal < 1)
+                    horizontal += Time.deltaTime;
+                break;
+            
             case 4:
                 brake = true;
                 break;
+        }
+        
+        if(sensors.colorLeft == 1 && sensors.colorMid == 1 && sensors.colorRight == 1)
+        {
+            reward = -1;
+            done = true;
+        }
+        else if(transform.position.z > 0)
+        {
+            reward = 1;
+            done = true;
+        }
+        else if (sensors.colorLeft != 0 && sensors.colorMid == 0 && sensors.colorRight != 0)
+        {
+            reward = 0.01f;
+        }
+        else
+        {
+            reward = -0.01f;
         }
     }
 
     public override void AgentReset()
     {
-        base.AgentReset();
+        transform.GetComponent<Rigidbody>().isKinematic = true;
+
+        horizontal = 0;
+        vertical = 0;
+
+        transform.position = new Vector3(0, 1, -40);
+        transform.rotation = Quaternion.identity;
+
+        transform.GetComponent<Rigidbody>().isKinematic = false;
     }
 }
---
+++ unity-environment/Assets/ML-Agents/Examples/1 - Self Driving Car/Scripts/SensoryRays.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensoryRays : MonoBehaviour
+{
+    public float colorMid;
+    public float colorRight;
+    public float colorLeft;
+
+    private RaycastHit rayMid;
+    private RaycastHit rayRight;
+    private RaycastHit rayLeft;
+
+	void Update ()
+    {
+        //mid
+        if (Physics.Raycast(transform.position, Quaternion.AngleAxis(10, Vector3.right) * transform.forward, out rayMid))
+        {
+            colorMid = rayMid.collider.gameObject.GetComponent<MeshRenderer>().material.color.grayscale;
+            Debug.DrawLine(transform.position, rayMid.point);
+        }
+
+        //right
+        if (Physics.Raycast(transform.position, Quaternion.AngleAxis(10, Vector3.right) * Quaternion.AngleAxis(30, Vector3.up) * transform.forward, out rayRight))
+        {
+            colorRight = rayRight.collider.gameObject.GetComponent<MeshRenderer>().material.color.grayscale;
+            Debug.DrawLine(transform.position, rayRight.point);
+        }
+
+        //left
+        if (Physics.Raycast(transform.position, Quaternion.AngleAxis(10, Vector3.right) * Quaternion.AngleAxis(-30, Vector3.up) * transform.forward, out rayLeft))
+        {
+            colorLeft = rayLeft.collider.gameObject.GetComponent<MeshRenderer>().material.color.grayscale;
+            Debug.DrawLine(transform.position, rayLeft.point);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(transform.position, new Vector3(0.2f, 0.2f, 0.2f));
+
+        Gizmos.DrawSphere(rayLeft.point, 0.1f);
+        Gizmos.DrawSphere(rayMid.point, 0.1f);
+        Gizmos.DrawSphere(rayRight.point, 0.1f);
+    }
+}
